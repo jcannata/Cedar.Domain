ae16b36fe63c5ec297fd534379a02c503a3886e6
thefringeninja <joao.paul@braganca.name>
2015-04-29 16:38:54 +0200

    the testing framework

---
+++ src/PowerAssert/PowerAssert/Infrastructure/Nodes/ConditionalNode.cs
@@ -0,0 +1,27 @@
+﻿namespace PowerAssert.Infrastructure.Nodes
+{
+    using JetBrains.Annotations;
+
+    internal class ConditionalNode : Node
+    {
+        [NotNull]
+        public Node Condition { get; set; }
+
+        [NotNull]
+        public Node TrueValue { get; set; }
+
+        [NotNull]
+        public Node FalseValue { get; set; }
+
+        internal override void Walk(NodeWalker walker, int depth)
+        {
+            walker("(");
+            Condition.Walk(walker, depth + 1);
+            walker(" ? ");
+            TrueValue.Walk(walker, depth + 1);
+            walker(" : ");
+            FalseValue.Walk(walker, depth + 1);
+            walker(")");
+        }
+    }
+}
